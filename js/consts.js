fef8328a019d54f60ed2cc451fc10d03bb32538e
Chris Zuber <shgysk8zer0@gmail.com>
2020-12-28 13:59:54 -0800

    Update consts.js

--- js/consts.js
+++ js/consts.js
@@ -1,1 +1,11 @@
 export const GA = null;
+
+export const env = (location.hostname === 'localhost' || location.hostname.endsWith('.netlify.live'))
+	? 'development'
+	: 'production';
+
+export const dev = env === 'development';
+
+export const site = {
+	title: 'Static Template',
+};
