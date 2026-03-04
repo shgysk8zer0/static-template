a35a5b1005cdba31488611efaae3bdf6bde63688
Chris Zuber <shgysk8zer0@gmail.com>
2026-03-04 15:12:28 -0800

    Bump Node, update importmap, CSP and deps
    Upgrade Node version (.nvmrc -> v24.10.0), refresh index.html importmap and polyfills to newer packages/versions, and switch to the built /js/index.min.js. Tighten/adjust Content-Security-Policy trusted-types entries in _headers and add credentialless to youtube-player. Remove getDefaultPolicy() invocation in js/index.js (left commented) and update package-lock (lockfileVersion -> 3) to reflect dependency upgrades.

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,13 @@
-/* eslint-env node */
 import { getConfig } from '@shgysk8zer0/js-utils/rollup';
 import { rollupImport, rollupImportMeta } from '@shgysk8zer0/rollup-import';
 import { readJSONFile } from '@shgysk8zer0/npm-utils/json';
+import { importmap } from '@shgysk8zer0/importmap';
 
 const { homepage } = await readJSONFile('./package.json');
 
 export default getConfig('./js/index.js', {
 	plugins: [
-		rollupImport('./importmap.json'),
+		rollupImport(importmap),
 		rollupImportMeta({ baseURL: homepage }),
 	],
 	format: 'iife',
